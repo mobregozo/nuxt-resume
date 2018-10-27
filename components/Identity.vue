e5d0a5d2217062926f624e62525936db7bbb820c
papyreno <papy.reno@gmail.com>
2018-10-27 18:08:05 +0200

    display identity section

---
+++ components/Identity.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="identity-container">
+    <div v-text="fullName"></div>
+  </section>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Identity',
+
+    computed: {
+      ...mapGetters([ 'getIdentity' ]),
+
+      fullName() {
+        return `${this.getIdentity.firstName} ${this.getIdentity.lastName}`
+      }
+    }
+  }
+</script>
