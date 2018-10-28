e88abbfc396dd7cd592f5111c32f9306eb1ea439
papyreno <papy.reno@gmail.com>
2018-10-28 18:08:25 +0100

    display hobbies section

---
+++ components/Hobbies.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hobbies-container">
+    <ul>
+      <li v-for="hobby in getHobbies" :key="hobby" v-text="hobby"></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Hobbies',
+
+    computed: {
+      ...mapGetters(['getHobbies'])
+    }
+  }
+</script>
