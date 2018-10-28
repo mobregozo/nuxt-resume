92760d09f79d6d11ddc8d3e3f47ec5590fd3bbed
papyreno <papy.reno@gmail.com>
2018-10-28 19:20:27 +0100

    init less styling

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,10 @@ module.exports = {
         })
       }
     }
-  }
+  },
+
+  css: [
+    './assets/less/style.less'
+  ]
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="resume-container">
     <identity />
     <description />
     <contact />
