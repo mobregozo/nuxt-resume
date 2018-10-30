111d1213f58831ecfe08a64aee2b20e750e916b3
papyreno <papy.reno@gmail.com>
2018-10-30 18:42:26 +0100

    display contact section

--- components/ContactPhone.vue
+++ components/ContactPhone.vue
@@ -1,25 +1,29 @@
 <template>
-  <ul id="phone-container" class="contact" v-if="hasPhone">
-    <li v-if="getContact.phone.home" v-text="getContact.phone.home"></li>
-    <li v-if="getContact.phone.cellular" v-text="getContact.phone.cellular"></li>
-    <li v-if="getContact.phone.pro" v-text="getContact.phone.pro"></li>
+  <ul id="phone-container" class="contact">
+    <li v-if="getContact.phone.home">
+      <contact-line type="phone" :value="getContact.phone.home" />
+    </li>
+    <li v-if="getContact.phone.mobile">
+      <a :href="`tel:${getContact.phone.mobile}`">
+        <contact-line type="mobile" :value="getContact.phone.mobile" />
+      </a>
+    </li>
   </ul>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import ContactLine from './ContactLine'
 
   export default {
     name: 'ContactPhone',
 
+    components: {
+      ContactLine
+    },
+
     computed: {
-      ...mapGetters([ 'getContact' ]),
-
-      hasPhone() {
-        const phone = this.getContact.phone
-
-        return phone.home || phone.cellular || phone.pro
-      }
+      ...mapGetters([ 'getContact' ])
     }
   }
 </script>
